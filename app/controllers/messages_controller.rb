f2ee4e361d86843fd1d4b68f49f727d7168b000e
cyang <cyang@netprotections.co.jp>
2016-06-14 07:00:16 +0000

    create message model

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,21 @@
 class MessagesController < ApplicationController
+  
   def index
+    # Messageモデルのオブジェクトの初期化
+    @message = Message.new
+    # Messageを全て取得する。get all msgs
+    @messages = Message.all
   end
+
+  def create
+    @message = Message.new(message_params)
+    @message.save
+    redirect_to root_path , notice: 'Message has been saved successfully!'
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:name, :body)
+  end
+  ## ここまで
 end
