956268a570c177a875dafc13a56a00b175910acd
cyang <cyang@netprotections.co.jp>
2016-06-15 08:19:35 +0000

    0615 L6-2 complete-3 @challenge2

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,5 +4,8 @@ class Message < ActiveRecord::Base
   # 内容は必須入力かつ2文字以上30文字以下
   validates :body , length: { minimum: 2, maximum: 30 } , presence: true
   # ageは必須入力かつ0文字以上
-  validates :age , length: { minimum: 0} , presence: true
+  # validates :age , length: { minimum: 0} , presence: true
+  validates :age , numericality: {
+             only_integer: true, greater_than_or_equal_to: 0
+             }
 end
